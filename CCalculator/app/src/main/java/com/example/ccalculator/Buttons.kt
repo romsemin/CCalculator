6abd055e14cdde27a90962927ab4129890c7dd45
Roman Semin <semin.roman.i@mail.ru>
2022-10-03 01:43:16 +0300

    cleaned duplicates

--- CCalculator/app/src/main/java/com/example/ccalculator/Buttons.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ccalculator
-
-enum class Buttons(val properties: ButtonsRow) {
-    FIRST(ButtonsRow("0", ".",  null, "=")),
-    SECOND(ButtonsRow("1", "2", "3", "+")),
-    THIRD(ButtonsRow("4", "5", "6", "-")),
-    FOURTH(ButtonsRow("7", "8", "9", "x")),
-    FIFTH(ButtonsRow("AC", null, null, "/"))
-}
--- CCalculator/app/src/main/java/com/example/ccalculator/CCalculatorInteractor.kt
+++
@@ -1,4 +0,0 @@
-package com.example.ccalculator
-
-class CCalculatorInteractor {
-}
